71e09cae15752e8ecb4915b7bf74c1f24da435e9
ktro2828 <kotaro.uetake@tier4.jp>
2023-08-23 10:21:03 +0900

    fix: update variance calculation
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- perception/patchworkpp/include/patchworkpp/patchworkpp.hpp
+++ perception/patchworkpp/include/patchworkpp/patchworkpp.hpp
@@ -172,9 +172,11 @@ private:
   {
     double mean = std::accumulate(values.begin(), values.end(), 0.0) / values.size();
 
-    double std_dev = std::accumulate(
-      values.begin(), values.end(), 0.0,
-      [&](double acc, const double & v) { return acc + std::pow(v - mean, 2.0); });
+    double std_dev =
+      std::accumulate(
+        values.begin(), values.end(), 0.0,
+        [&](double acc, const double & v) { return acc + std::pow(v - mean, 2.0); }) /
+      values.size();
 
     return {mean, std_dev};
   }
