933f00e80fcc24796cc95bf2d555a3ce46fd8679
ktro2828 <kotaro.uetake@tier4.jp>
2023-09-13 00:24:19 +0900

    fix: fix parameter definition
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- perception/patchworkpp/src/params.cpp
+++ perception/patchworkpp/src/params.cpp
@@ -23,14 +23,11 @@ namespace patchwork_pp
 CommonParams::CommonParams(rclcpp::Node * node)
 {
   sensor_height_ = node->declare_parameter<double>("common.sensor_height");
-  min_range_ = node->declare_parameter<double>("common.min_range");
-  max_range_ = node->declare_parameter<double>("common.max_range");
   seed_selection_weight_ = node->declare_parameter<double>("common.seed_selection_weight");
 }
 
 RNRParams::RNRParams(rclcpp::Node * node)
 {
-  num_sample_ = node->declare_parameter<int>("rnr.num_sample");
   min_height_threshold_ = node->declare_parameter<double>("rnr.min_height_threshold");
   min_vertical_angle_threshold_ =
     node->declare_parameter<double>("rnr.min_vertical_angle_threshold");
@@ -39,40 +36,34 @@ RNRParams::RNRParams(rclcpp::Node * node)
 
 CZMParams::CZMParams(rclcpp::Node * node)
 {
-  num_zone_ = node->declare_parameter<int>("czm.num_zone");
-  num_near_ring_ = node->declare_parameter<int>("czm.num_near_ring");
-  min_num_point_ = node->declare_parameter<int>("czm.min_num_point");
-  min_zone_ranges_ = node->declare_parameter<std::vector<double>>("czm.min_zone_ranges");
+  min_range_ = node->declare_parameter<double>("czm.min_range");
+  max_range_ = node->declare_parameter<double>("czm.max_range");
+  num_near_ring_ = static_cast<size_t>(node->declare_parameter<int>("czm.num_near_ring"));
+  min_num_point_ = static_cast<size_t>(node->declare_parameter<int>("czm.min_num_point"));
   num_sectors_ = node->declare_parameter<std::vector<int64_t>>("czm.num_sectors");
   num_rings_ = node->declare_parameter<std::vector<int64_t>>("czm.num_rings");
   elevation_thresholds_ = node->declare_parameter<std::vector<double>>("czm.elevation_thresholds");
   flatness_thresholds_ = node->declare_parameter<std::vector<double>>("czm.flatness_thresholds");
 
-  if (num_zone_ != static_cast<int>(min_zone_ranges_.size())) {
-    RCLCPP_ERROR_STREAM(
-      node->get_logger(),
-      "Expected `min_zone_ranges` has the same number of elements as `num_zone`, but got "
-        << min_zone_ranges_.size() << " and " << num_zone_);
-  }
-  if (num_zone_ != static_cast<int>(num_sectors_.size())) {
+  if (num_zone_ != num_sectors_.size()) {
     RCLCPP_ERROR_STREAM(
       node->get_logger(),
       "Expected `num_sectors` has the same number of elements as `num_zone`, but got "
         << num_sectors_.size() << " and " << num_zone_);
   }
-  if (num_zone_ != static_cast<int>(num_rings_.size())) {
+  if (num_zone_ != num_rings_.size()) {
     RCLCPP_ERROR_STREAM(
       node->get_logger(),
       "Expected `num_rings` has the same number of elements as `num_zone`, but got "
         << num_rings_.size() << " and " << num_zone_);
   }
-  if (num_near_ring_ != static_cast<int>(elevation_thresholds_.size())) {
+  if (num_near_ring_ != elevation_thresholds_.size()) {
     RCLCPP_ERROR_STREAM(
       node->get_logger(),
       "Expected `elevation_thresholds` has the same number of elements as `num_near_ring`, but got "
         << elevation_thresholds_.size() << " and " << num_zone_);
   }
-  if (num_near_ring_ != static_cast<int>(flatness_thresholds_.size())) {
+  if (num_near_ring_ != flatness_thresholds_.size()) {
     RCLCPP_ERROR_STREAM(
       node->get_logger(),
       "Expected `flatness_thresholds` has the same number of elements as `num_near_ring`, but got "
@@ -83,9 +74,20 @@ CZMParams::CZMParams(rclcpp::Node * node)
     sector_sizes_.push_back(2 * M_PI / num);
   }
 
-  for (int i = 1; i < num_zone_; ++i) {
-    ring_sizes_.push_back(
-      min_zone_ranges_.at(i) - min_zone_ranges_.at(i - 1) / num_rings_.at(i - 1));
+  {
+    min_zone_ranges_.resize(num_zone_);
+    min_zone_ranges_.at(0) = min_range_;
+    min_zone_ranges_.at(1) = (7.0 * min_range_ + max_range_) / 8.0;
+    min_zone_ranges_.at(2) = (3.0 * min_range_ + max_range_) / 4.0;
+    min_zone_ranges_.at(3) = (min_range_ + max_range_) / 2.0;
+  }
+
+  {
+    ring_sizes_.resize(num_zone_);
+    ring_sizes_.at(0) = (min_zone_ranges_.at(1) - min_zone_ranges_.at(0)) / num_rings_.at(0);
+    ring_sizes_.at(1) = (min_zone_ranges_.at(2) - min_zone_ranges_.at(1)) / num_rings_.at(1);
+    ring_sizes_.at(2) = (min_zone_ranges_.at(3) - min_zone_ranges_.at(2)) / num_rings_.at(2);
+    ring_sizes_.at(3) = (max_range_ - min_zone_ranges_.at(3)) / num_rings_.at(3);
   }
 }
 
@@ -95,8 +97,8 @@ RPFParams::RPFParams(rclcpp::Node * node)
     node->declare_parameter<double>("rpf.max_vertical_distance_threshold");
   max_angle_threshold_ = node->declare_parameter<double>("rpf.max_angle_threshold");
   max_distance_threshold_ = node->declare_parameter<double>("rpf.max_distance_threshold");
-  num_iterator_ = node->declare_parameter<int>("rpf.num_iterator");
-  num_sample_ = node->declare_parameter<int>("rpf.num_sample");
+  num_iterator_ = static_cast<size_t>(node->declare_parameter<int>("rpf.num_iterator"));
+  num_sample_ = static_cast<size_t>(node->declare_parameter<int>("rpf.num_sample"));
 }
 
 GLEParams::GLEParams(rclcpp::Node * node)
